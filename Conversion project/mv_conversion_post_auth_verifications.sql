3a6dd767557154d0ebfa52f036f3acede263a2bb
Nirplex <nir@simplex.com>
2017-12-20 17:45:12 +0200

    more conversion!

--- Conversion project/mv_conversion_post_auth_verifications.sql
+++ Conversion project/mv_conversion_post_auth_verifications.sql
@@ -1,14 +1,6 @@
 CREATE MATERIALIZED VIEW mv_conversion_post_auth_verifications AS
 
-with p_ids as (
-  select
-    id
-  from
-    payments
-  WHERE
-    created_at between now()-interval '37 days' and now()-interval '7 days'),
-
-verification_requests_and_answers AS
+with verification_requests_and_answers AS
 (
   SELECT
     vr.inserted_at,
@@ -36,7 +28,7 @@ verification_requests_and_answers AS
       else false end as is_clarification_requested
   FROM
     verification_requests vr,
-    p_ids
+    mv_conversion_payment_ids p_ids
   WHERE
     p_ids.id = vr.payment_id
 ),
@@ -72,7 +64,7 @@ verification_requests_and_answers AS
       (
         SELECT
           gvraa.payment_id,
-          last_value(v.status) OVER (PARTITION BY payment_id ORDER BY payment_id DESC) AS status
+          first_value(v.status) OVER (PARTITION BY payment_id  ORDER BY id desc) AS status
         FROM
           grouped_verification_requests_and_answers gvraa,
           verifications v
@@ -83,7 +75,6 @@ verification_requests_and_answers AS
       ) kyc_identity_status_before_grouping
     GROUP BY 1,2
   ),
-
 
   selfie_status as
   (
@@ -104,45 +95,6 @@ verification_requests_and_answers AS
   ),
 
 
-  cancelled_payments as
-  (
-    SELECT
-      DISTINCT on (d.payment_id)
-      d.payment_id,
-      CASE
-         when application_name = 'EndUser' and vr.allow_verifications#>>'{0}' in ('photo_selfie', 'video_selfie') then
-      'user_cancelled_selfie'
-      when application_name = 'EndUser' and vr.allow_verifications#>>'{0}' in ('kyc_identity') then
-        'user_cancelled_kyc'
-      when application_name = 'Scheduler' and vr.allow_verifications#>>'{0}' in  ('photo_selfie', 'video_selfie') then
-        'scheduler_cancelled_selfie'
-      when application_name = 'Scheduler' and vr.allow_verifications#>>'{0}' in ('kyc_identity') then
-         'scheduler_cancelled_kyc'
-      when d.payment_id in (select
-                            payment_id from comments where text_data = 'Cancelled because of change in BTC rate (broker policy)') then 'cancelled_due_to_quote'
-
-      when application_name = 'Manual'  then
-         'manual_cancelled'
-      when application_name = 'Scheduler'  then
-         'scheduler_cancelled_bug'
-      when application_name = 'Bender_Manual'  then
-         'batch_cancelled'
-
-  else 'other' end as cancellation_reason
-
-    FROM
-      decisions d,
-      verification_requests vr,
-      p_ids
-      where
-        p_ids.id = vr.payment_id
-        and vr.payment_id = d.payment_id
-        and d.application_name in ('EndUser', 'Scheduler', 'Manual', 'Bender_Manual')
-        AND d.decision = 'cancelled'
-    order by d.payment_id desc, vr.inserted_at DESC
-  ),
-
-
 verification_statuses as
   (
     SELECT
@@ -158,7 +110,7 @@ verification_statuses as
   WHEN gvraa.is_kyc_identity_requested = TRUE AND kis.status IN ('expired', 'in_progress') THEN 'did not respond'
   WHEN gvraa.is_kyc_identity_requested = TRUE and gvraa.is_kyc_identity_returned = FALSE AND kis.status ISNULL THEN 'did not respond'
   WHEN gvraa.is_kyc_identity_requested = FALSE AND kis.status ISNULL THEN 'not relevant'
-  ELSE 'unexpected value' END AS kyc_status,
+  ELSE 'unexpected_value' END AS kyc_status,
 
   CASE
   WHEN gvraa.is_selfie_requested = TRUE and ss.selfie_decision in ('approved') THEN 'success'
@@ -166,7 +118,7 @@ verification_statuses as
   WHEN gvraa.is_selfie_requested = TRUE and (gvraa.how_many_selfies_required-how_many_selfies_returned)>0 AND ss.selfie_decision::text in ('no selfie decision') THEN 'did not respond'
   WHEN gvraa.is_selfie_requested = TRUE and (gvraa.how_many_selfies_required-how_many_selfies_returned)=0 AND ss.selfie_decision::text in ('no selfie decision') THEN 'selfie was not checked'
   WHEN gvraa.is_selfie_requested = FALSE AND ss.selfie_decision in ('no selfie decision') THEN 'not relevant'
-  ELSE 'unexpected value' END AS selfie_status,
+  ELSE 'unexpected_value' END AS selfie_status,
 
   cp.cancellation_reason
 
@@ -181,12 +133,10 @@ FROM
   ON
     ss.payment_id = gvraa.payment_id
   LEFT JOIN
-  cancelled_payments cp
+  mv_conversion_cancelled_payments cp
   ON
     cp.payment_id = gvraa.payment_id
-ORDER BY 1 DESC
-
-  )
+ORDER BY 1 DESC)
 
 
 SELECT
@@ -202,29 +152,29 @@ SELECT
   when selfie_status in ('declined', 'did not respond', 'selfie was not checked') then 'failed'
   when cancellation_reason is not null then 'failed'
 
-  ELSE 'unexpected value' END AS payment_status_in_stage,
+  ELSE 'unexpected_value' END AS payment_status_in_stage,
 
   CASE
-  when is_selfie_requested = TRUE and is_kyc_identity_requested = TRUE and kyc_status = 'success' and selfie_status = 'success' then 'kyc + selfie: approved both'
-  when is_selfie_requested = TRUE and is_kyc_identity_requested = FALSE and selfie_status = 'success' then 'selfie: approved'
-  when is_selfie_requested = FALSE and is_kyc_identity_requested = TRUE and kyc_status = 'success' then 'kyc: approved'
+  when is_selfie_requested = TRUE and is_kyc_identity_requested = TRUE and kyc_status = 'success' and selfie_status = 'success' then 'kyc_and_selfie:_approved_both'
+  when is_selfie_requested = TRUE and is_kyc_identity_requested = FALSE and selfie_status = 'success' then 'selfie:_approved'
+  when is_selfie_requested = FALSE and is_kyc_identity_requested = TRUE and kyc_status = 'success' then 'kyc:_approved'
 
-  when is_selfie_requested = TRUE and is_kyc_identity_requested = TRUE and kyc_status in ('declined') then 'kyc + selfie: kyc declined by partner'
-  when is_selfie_requested = TRUE and is_kyc_identity_requested = TRUE and kyc_status in ('success') and selfie_status in ('declined') then 'kyc + selfie: selfie declined'
-  when is_selfie_requested = TRUE and is_kyc_identity_requested = TRUE and cancellation_reason is not null then concat('kyc + selfie: ', cancellation_reason)
-  when is_selfie_requested = TRUE and is_kyc_identity_requested = TRUE and kyc_status in ('success') and selfie_status in ('did not respond') then 'kyc + selfie: not cancelled, selfie did not respond'
-  when is_selfie_requested = TRUE and is_kyc_identity_requested = TRUE and kyc_status in ('did not respond') then 'kyc + selfie: not cancelled, kyc did not respond'
+  when is_selfie_requested = TRUE and is_kyc_identity_requested = TRUE and kyc_status in ('declined') then 'kyc_and_selfie:_kyc_declined_by_partner'
+  when is_selfie_requested = TRUE and is_kyc_identity_requested = TRUE and kyc_status in ('success') and selfie_status in ('declined') then 'kyc_and_selfie:_selfie_declined'
+  when is_selfie_requested = TRUE and is_kyc_identity_requested = TRUE and cancellation_reason is not null then concat('kyc_and_selfie:_', cancellation_reason)
+  when is_selfie_requested = TRUE and is_kyc_identity_requested = TRUE and kyc_status in ('success') and selfie_status in ('did not respond') then 'kyc_and_selfie:_not_cancelled_selfie_did_not_respond'
+  when is_selfie_requested = TRUE and is_kyc_identity_requested = TRUE and kyc_status in ('did not respond') then 'kyc_and_selfie:_not_cancelled_kyc_did_not_respond'
 
 
-  when is_selfie_requested = TRUE and is_kyc_identity_requested = FALSE and selfie_status in ('declined') then 'selfie: selfie declined'
-  when is_selfie_requested = TRUE and is_kyc_identity_requested = FALSE and cancellation_reason is not null then concat('selfie: ', cancellation_reason)
-  when is_selfie_requested = TRUE and is_kyc_identity_requested = FALSE and selfie_status in ('did not respond') then 'selfie: not cancelled, selfie did not respond'
+  when is_selfie_requested = TRUE and is_kyc_identity_requested = FALSE and selfie_status in ('declined') then 'selfie:_selfie_declined'
+  when is_selfie_requested = TRUE and is_kyc_identity_requested = FALSE and cancellation_reason is not null then concat('selfie:_', cancellation_reason)
+  when is_selfie_requested = TRUE and is_kyc_identity_requested = FALSE and selfie_status in ('did not respond') then 'selfie:_not_cancelled_selfie_did_not_respond'
 
-  when is_selfie_requested = FALSE and is_kyc_identity_requested = TRUE and kyc_status in ('declined') then 'kyc: kyc declined by partner'
-  when is_selfie_requested = FALSE and is_kyc_identity_requested = TRUE and cancellation_reason is not null then concat('kyc: ', cancellation_reason)
-  when is_selfie_requested = FALSE and is_kyc_identity_requested = TRUE and kyc_status in ('did not respond') then 'kyc: not cancelled, kyc did not respond'
+  when is_selfie_requested = FALSE and is_kyc_identity_requested = TRUE and kyc_status in ('declined') then 'kyc:_kyc_declined_by_partner'
+  when is_selfie_requested = FALSE and is_kyc_identity_requested = TRUE and cancellation_reason is not null then concat('kyc:_', cancellation_reason)
+  when is_selfie_requested = FALSE and is_kyc_identity_requested = TRUE and kyc_status in ('did not respond') then 'kyc:_not_cancelled_kyc_did_not_respond'
 
-  ELSE 'unexpected value' END AS status_reason_in_stage
+  ELSE 'unexpected_value' END AS status_reason_in_stage
 FROM
   verification_statuses;
 
